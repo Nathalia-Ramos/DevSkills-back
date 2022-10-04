0591b80d5f33c452c5952fc817bb95c0748f9566
Leila Rosa <leila_rosa3@outlook.com>
2022-10-04 10:58:09 -0300

    refactor: ajustes no server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import * as dotenv from "dotenv";
+
+dotenv.config()
+
 //inicializando app
 const app = express();
 
@@ -7,18 +11,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 import UserRoutes from "../routes/CompanyRoutes/UserCompanyRoutes"
 import AddresRoutes from "../routes/CompanyRoutes/AddressRoutes" 
 import AuthRoutes from "../routes/CompanyRoutes/AuthRoutes"
 import ForgetPassword from "../routes/CompanyRoutes/ForgotPassword"
 import ResetPass  from "../routes/CompanyRoutes/ResetPassword"
+import Gender from "../routes/CommonRoutes/GenderRoutes"
+import DeveloperRoutes from "../routes/DeveloperRoutes/UserDeveloperRoutes";
 
 app.use('/user', UserRoutes)
 app.use('/teste', AddresRoutes)
 app.use('/auth', AuthRoutes)
 app.use('/forgot_pass', ForgetPassword)
 app.use('/reset', ResetPass)
+app.use('/developer', DeveloperRoutes)
+app.use('/gender', Gender)
 
 app.listen(8080, () => {
     console.log("Server running")
