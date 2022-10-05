6e47d22d56b171859c7bd6ac8675b51f6b9066f9
Leila Rosa <leila_rosa3@outlook.com>
2022-10-05 11:32:45 -0300

    feat: adiciona endpoints para tblstacks

---
+++ src/api/controller/Stacks/StackController.ts
@@ -0,0 +1,25 @@
+import {Request, Response} from "express";
+import StackModel from "../../models/Stack/StackModel";
+import message from "../../../config/ReturnMessages";
+import validateRegex from "../../../utils/ValidateRegex";
+
+export default class StackController{
+    static async execute(req: Request, res: Response){
+        
+        const { nome } = req.body;
+
+        if (validateRegex(nome, '^[a-zA-Z ]*$')) {
+            try {
+                const newStack = await StackModel.execute({nome})
+                return res.status(200).json({message: message.Success, dados: newStack})
+            } catch (error) {
+                console.log(error)
+                return res.send({message: message.Conflict})
+            }
+        } else {
+            return res.send({message: message.Conflict})
+        }
+       
+    }
+
+}
---
+++ src/api/models/Stack/StackModel.ts
@@ -0,0 +1,47 @@
+import { PrismaClient, Stack } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+interface StackData {
+    nome: string,
+}
+
+export default class StackModel{
+      
+    static async execute({
+        nome,
+    }: StackData) : Promise<Stack | boolean> {
+        
+        try {
+          const newStack = await prisma.stack.create({
+            data:{
+                nome,
+            },
+          });
+    
+          prisma.$disconnect;
+    
+          return newStack;
+          
+        } catch (error) {
+          console.error(error);
+    
+          prisma.$disconnect;
+    
+          return false;
+        }
+      }
+
+    static async showAll() {
+      try {
+        const getAll = await prisma.stack.findMany() 
+        
+        prisma.$disconnect;
+
+        return getAll;
+      } catch (error) {
+        prisma.$disconnect;
+        return error
+      }
+    }
+    }
