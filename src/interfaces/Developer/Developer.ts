577ff5d61cbe13de8cb871bf9b0802bb8a95f727
Leila <leila_rosa3@outlook.com>
2022-10-21 15:54:29 -0300

    fix: arrumando conflitos

--- src/interfaces/Developer/Developer.ts
+++ src/interfaces/Developer/Developer.ts
@@ -3,7 +3,6 @@ export default interface DeveloperData {
         email: string,
         cpf: string,    
         data_nascimento: string, // ANO/MES/DIA
-        permissao_email: boolean,
         
         id_genero: number,
 }
