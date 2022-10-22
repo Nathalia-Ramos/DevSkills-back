f636e496d834933f9d40c9f42d370e032ab2e50f
Leila <81823279+rosaaleila@users.noreply.github.com>
2022-10-22 16:49:15 -0300

    fix: arrumando migrations

--- prisma/migrations/20221022194719_ajustes/migration.sql
+++ prisma/migrations/20221022194719_ajustes/migration.sql
@@ -1,8 +1,8 @@
 /*
   Warnings:
 
-  - You are about to alter the column `data_solicitacao` on the `tblDenuncia` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
-  - You are about to alter the column `data_solucao` on the `tblDenuncia` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `data_solicitacao` on the `tbldenuncia` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `data_solucao` on the `tbldenuncia` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
   - You are about to alter the column `condicao` on the `tblemblema` table. The data in that column could be lost. The data in that column will be cast from `Decimal(10,0)` to `Decimal`.
   - You are about to alter the column `data_pagamento` on the `tblpagamento` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
   - You are about to alter the column `juros` on the `tblpagamento` table. The data in that column could be lost. The data in that column will be cast from `Decimal(10,0)` to `Decimal`.
@@ -11,11 +11,14 @@
   - You are about to alter the column `data_inicio` on the `tblprova_andamento` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
   - You are about to alter the column `data_fim` on the `tblprova_andamento` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
   - You are about to alter the column `data_entrega` on the `tblusuario_prova` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
-  - Added the required column `permissao_email` to the `tblusuario` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `id_questao` to the `tblalternativa_prova` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE `tblDenuncia` MODIFY `data_solicitacao` DATETIME NOT NULL,
+ALTER TABLE `tblalternativa_prova` ADD COLUMN `id_questao` INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE `tbldenuncia` MODIFY `data_solicitacao` DATETIME NOT NULL,
     MODIFY `data_solucao` DATETIME NULL;
 
 -- AlterTable
@@ -33,11 +36,10 @@ ALTER TABLE `tblprova` MODIFY `ultima_atualizacao` DATETIME NULL;
 
 -- AlterTable
 ALTER TABLE `tblprova_andamento` MODIFY `data_inicio` DATETIME NOT NULL,
-    MODIFY `data_fim` DATETIME NOT NULL,
-    MODIFY `duracao` TIME NULL;
-
--- AlterTable
-ALTER TABLE `tblusuario` ADD COLUMN `permissao_email` BOOLEAN NOT NULL;
+    MODIFY `data_fim` DATETIME NOT NULL;
 
 -- AlterTable
 ALTER TABLE `tblusuario_prova` MODIFY `data_entrega` DATETIME NULL;
+
+-- AddForeignKey
+ALTER TABLE `tblalternativa_prova` ADD CONSTRAINT `tblalternativa_prova_id_questao_fkey` FOREIGN KEY (`id_questao`) REFERENCES `tblquestao_prova`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
