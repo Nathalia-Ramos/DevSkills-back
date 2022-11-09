767b9b6e2ea6ec2167501c7febee8bdbeea2535a
Leila <leila_rosa3@outlook.com>
2022-11-09 14:40:36 -0300

    chore: ajustando rota de listagem

--- routes/TestRoutes/TestRouter.ts
+++ routes/TestRoutes/TestRouter.ts
@@ -4,7 +4,7 @@ import TestController from "../../src/api/controller/Test/TestController"
 const router = Router()
 
 router.post('/', TestController.execute)
-router.get('/tests', TestController.test)
+router.get('/allTest', TestController.test)
 router.post('/template', TestController.relateTestTemplate) 
 router.get('/admin', TestController.findAdminTests)
 router.get('/listTest', TestController.listTest)
